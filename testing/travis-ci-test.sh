de6f653c1ac3a7d1956d3a8be7717ac8a05f5703
Fulvio Carvalhido <inluxc@gmail.com>
2021-02-14 10:38:02 +0000

    Update travis-ci-test.sh

--- testing/travis-ci-test.sh
+++ testing/travis-ci-test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Run tests under Travis for continuous integration.
 
+
+sudo chown $USER:$USER ~/.Xauthority
+
 go test -coverprofile=coverage.txt -covermode=atomic -test.v -timeout=20m ./...
